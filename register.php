924204ddab0922c6b93e2f19db8cf44c19952c7a
Sambhav Gautam <buffgautam22@gmail>
2026-07-23 17:29:07 +0545

    created 80% backend(authentication logic)

---
+++ register.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'config/db_connect.php'
+
+if($_SERVER["REQUEST_METHOD"]== "POST"){
+
+    // retrive the data from the html 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password']
+    $confirm_password = $_POST['confirm_password']
+
+    if(empty('name') || empty('email') || empty('phone') || empty('password')){
+        $error "Please fill all the required fields"
+    }
+
+    elseif($password !== $confirm_password){
+        $error "The filled up password do not match"
+    }
+
+    elseif (strlen($password)<6 ){
+        $error "The password need to be above six characters."
+    }
+
+    else{
+        $check_email = "SELECT * FROM customers WHERE C_Email = '$email'";
+        $check_result = $conn -> query($check_email);
+
+        if($check_result-> num_rows > 0 ){
+            $error "The email already exists";
+        }
+        else{
+            $hashed_password = hash_password($password, PASSWORD_DEFAULT)
+        }
+    }
+
+
+} 
